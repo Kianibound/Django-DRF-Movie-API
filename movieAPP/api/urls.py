31e86e8d7b0e1c6a441e45b55830b36571cbb652
kianibound <kianibound@gmail.com>
2025-10-10 13:34:48 +0330

    change views into class based views

--- movieAPP/api/urls.py
+++ movieAPP/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from movieAPP.api.views import movie_details, movies_list
+from movieAPP.api.views import MovieListAV, MovieDetailAV
 
 urlpatterns = [
-    path('list/', movies_list, name='movies-list'),
-    path('<int:pk>/', movie_details, name='movie-detail'),
+    path('list/', MovieListAV.as_view(), name='movies-list'),
+    path('<int:pk>/', MovieDetailAV.as_view(), name='movie-detail'),
 ]
