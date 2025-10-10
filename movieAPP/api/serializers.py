2302781b9be34f1ad590186c4bb309c5dffb27fd
kianibound <kianibound@gmail.com>
2025-10-10 13:53:53 +0330

    add some validators

--- movieAPP/api/serializers.py
+++ movieAPP/api/serializers.py
@@ -1,19 +1,37 @@
+from django.forms import ValidationError
 from rest_framework import serializers
 from movieAPP.models import Movie
 
 
+def name_length(value):
+    if len(value) < 3:
+        raise serializers.ValidationError("Name is Too Short!")
+
+
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
+    name = serializers.CharField(validators=[name_length])
     description = serializers.CharField()
     active = serializers.BooleanField()
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
-      
+
     def update(self, instance, validated_data):
-      instance.name = validated_data.get('name', instance.name)
-      instance.description = validated_data.get('description', instance.description)
-      instance.active = validated_data.get('active', instance.active)
-      instance.save()
-      return instance
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get(
+            'description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
+
+    def validate(self, data):
+        if data['name'] == data['description']:
+            raise serializers.ValidationError(
+                "Name and Description Should Be Different")
+        return data
+
+    # def validate_name(self, value):
+    #     if len(value) < 3:
+    #         raise serializers.ValidationError("Name is Too Short!")
+    #     return value
