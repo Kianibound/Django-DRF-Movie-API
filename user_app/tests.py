331a5571eb59660806e9974aa436e5d41f57884e
kianibound <kianibound@gmail.com>
2025-10-16 15:22:53 +0330

    Add tests for register, login, and logout in user_app

--- user_app/tests.py
+++ user_app/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework import response, status
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+
+class RegisterTestCase(APITestCase):
+
+    def test_register(self):
+        data = {
+            "email": "1@1.com",
+            "username": "test",
+            "password": "test",
+            "password2": "test"
+        }
+        response = self.client.post(reverse("register"), data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class LoginLogoutTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="test", password="test")
+
+    def test_login(self):
+        data = {
+            "username": "test",
+            "password": "test"
+        }
+        response = self.client.post(reverse("login"), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_logout(self):
+        self.token = Token.objects.get(user__username="test")
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+        response = self.client.post(reverse("logout"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
